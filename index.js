f87bc1bd72a7112a21ca9d972ecc6af098704bc1
MW1R1 <nickmwirigi58@gmail.com>
2023-03-17 12:32:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = 'Grace Hopper';
+let WelcomeMessage = 'Welcome to Flatbook';
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
